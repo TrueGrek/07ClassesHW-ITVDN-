2dca9896d8610da456514f38d240841b77303f07
Grek <grrekov@yandex.ru>
2020-03-17 21:08:24 +0300

    Добавьте файлы проекта.

---
+++ 07ClassesHW/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07ClassesHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook note = new Notebook("M3", "Tesla", 150000);
+            note.Show();
+        }
+    }
+    struct Notebook
+    {
+        private string model;
+        private string productManager;
+        private int cost;
+
+        public Notebook(string model, string productManager, int cost)
+        {
+            this.model = model;
+            this.productManager = productManager;
+            this.cost = cost;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Model: {0}\nProduct: {1}\nCost: {2}", model, productManager, cost);
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass testclass = new MyClass();
+            MyStruct teststruct = new MyStruct();
+
+            testclass.change = "не изменено";
+            teststruct.change = "не изменено";
+
+            ClassTaker(testclass);
+            StructTaker(ref teststruct);//Ничего не изменится так как мы передаем не ссылку, а значение.
+            //Для того чтобы изменить значение в структуре, применяем ref.
+            //Тогда мы передаём ссылку и меняем её.
+
+            Console.WriteLine("Class - {0}", testclass.change);
+            Console.WriteLine("Struct - {0}", teststruct.change);
+        }
+        static void ClassTaker(MyClass c)
+        {
+            c.change = "изменено";
+        }
+        static void StructTaker(ref MyStruct s)
+        {
+            s.change = "изменено";
+        }
+
+    }
+
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+
+    
+}
